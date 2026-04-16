ccbd497fd3ebdcee567bc15f98e5e0f0b51cab2d
Odhisika <francisganyo64@gmail.com>
2026-04-16 18:05:40 +0000

    cart update

--- store/templatetags/custom_filters.py
+++ store/templatetags/custom_filters.py
@@ -20,3 +20,7 @@ def trim(value):
     if isinstance(value, str):
         return value.strip()
     return value
+
+@register.simple_tag
+def get_elided_page_range(paginator, number, on_each_side=1, on_ends=1):
+    return paginator.get_elided_page_range(number, on_each_side=on_each_side, on_ends=on_ends)
